cb0ec7a7c96c9e8e7c8d605b133a2be59a71cef0
fengming.shi <fengming.shi@ele.me>
2017-09-25 12:31:17 +0800

    修改头文件

--- LPDSoundServiceKit.podspec
+++ LPDSoundServiceKit.podspec
@@ -36,7 +36,6 @@ TODO: Add long description of the pod here.
   #   'LPDSoundServiceKit' => ['LPDSoundServiceKit/Assets/*.png']
   # }
 
-  s.public_header_files = 'Pod/Classes/LPDSoundServiceKit.h'
   s.frameworks = 'UIKit', 'AudioToolbox', 'AVFoundation', 'MediaPlayer', 'CoreTelephony', 'CallKit'
   s.dependency 'ReactiveObjC'
 end
--- LPDSoundServiceKit/Classes/LPDSoundServiceKit.h
+++ LPDSoundServiceKit/Classes/LPDSoundServiceKit.h
@@ -5,11 +5,15 @@
 //  Created by PixelShi on 2017/9/25.
 //
 
-#ifndef LPDSoundServiceKit_h
-#define LPDSoundServiceKit_h
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactiveObjC.
+FOUNDATION_EXPORT double LPDSoundServiceKitVersionNumber;
+
+//! Project version string for ReactiveObjC.
+FOUNDATION_EXPORT const unsigned char LPDSoundServiceKitVersionString[];
 
 #import <LPDSoundServiceKit/LPDSoundService.h>
 #import <LPDSoundServiceKit/LPDTeleponyManager.h>
 #import <LPDSoundServiceKit/LPDVolumeManager.h>
 
-#endif /* LPDSoundServiceKit_h */
